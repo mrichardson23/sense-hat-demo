54e40b6e49f28b5dc613a3949b7ac4e14488bdad
Matt Richardson <mrichardson23@gmail.com>
2016-03-08 13:25:43 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
+# Get code
+git clone https://github.com/mrichardson23/maker-faire-sense-hat-demo.git
+
 # Install evdev
 sudo apt-get install -y python-dev python-pip gcc
 sudo pip install evdev
@@ -6,7 +9,7 @@ sudo pip install evdev
 sudo raspi-config nonint do_boot_behaviour_new B2
 
 # Set to start on boot:
-echo "#!/bin/sh -e" > /etc/rc.local
-echo "python /home/pi/maker-faire-sense-hat-demo/maker-faire.py &" >> /etc/rc.local
-echo "exit 0" >> /etc/rc.local
+sudo echo "#!/bin/sh -e" > /etc/rc.local
+sudo echo "python /home/pi/maker-faire-sense-hat-demo/maker-faire.py &" >> /etc/rc.local
+sudo echo "exit 0" >> /etc/rc.local
 
